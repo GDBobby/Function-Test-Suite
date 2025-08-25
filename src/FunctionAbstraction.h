3a496258012111e2d9d788212018c422efe9de2e
Corey <corey.w108@gmail.com>
2025-08-24 22:31:02 -0500

    Add batch benchmarking to FunctionAbstraction
    Introduces batch benchmarking methods to the FunctionAbstraction utility, allowing performance measurement over multiple iterations in batches. Refactors and namespaces the benchmarking code, adds utility functions for optimization prevention, and updates main.cpp to demonstrate and test the new batch benchmarking features, including a Noop benchmark.

--- src/FunctionAbstraction.h
+++ src/FunctionAbstraction.h
@@ -9,134 +9,210 @@
 
 #include <chrono>
 
-template<typename T>
-concept Printable = requires(T t) {
-    { t.Print() } -> std::same_as<void>;
-};
+#ifdef _MSC_VER
+#define STRINGIFY(x) #x
+#define TOSTRING(x) STRINGIFY(x)
 
-template <typename T>
-struct function_traits;
+#pragma message(__FILE__ "(" TOSTRING(__LINE__) "): Benchmarking a void function (not Noop)")
+#endif
 
-template <typename Result, typename... Args>
-struct function_traits<Result(*)(Args...)> {
-    using result_type = Result;
-    using args_tuple  = std::tuple<Args...>;
-    static constexpr std::size_t arity = sizeof...(Args);
-};
-template <typename Func>
-using first_arg_t = std::tuple_element_t<0, typename function_traits<std::decay_t<Func>>::args_tuple>;
+namespace BATS {
 
 
-template<uint16_t Count>
-struct BenchmarkResults {
-    std::array<std::chrono::nanoseconds, Count> total;
-    std::array<std::chrono::nanoseconds, Count> max;
-    std::array<std::chrono::nanoseconds, Count> min;
-    
-    const uint64_t iteration_count;
 
-	BenchmarkResults(uint64_t iteration_count) : iteration_count(iteration_count) {
-		total.fill(std::chrono::nanoseconds(0));
-		max.fill(std::chrono::nanoseconds(0));
-		min.fill(std::chrono::nanoseconds::max());
+    template<typename T>
+    inline void MAX_EQUALS(T& a, T const& b) {
+		a = a > b ? a : b;
+    }
+	template<typename T>
+	inline void MIN_EQUALS(T& a, T const& b) {
+		a = a < b ? a : b;
 	}
 
-	void Add(std::array<std::chrono::nanoseconds, Count> const& durations) {
-		for (std::size_t i = 0; i < Count; ++i) {
-			total[i] += durations[i];
-			max[i] = std::max(max[i], durations[i]);
-			min[i] = std::min(min[i], durations[i]);
-		}
-	}
-};
+    static uint64_t un_optimizer = 0;
+    template <class T>
+    inline void DoNotOptimize(T const& value) {
+		un_optimizer ^= reinterpret_cast<uint64_t>(&value);
+    }
 
-template<typename Result, auto... Funcs>
-struct FunctionAbstraction{
-    static constexpr auto funcs = std::tuple{Funcs...};
-    
-    template <std::size_t I, typename... Args>
-    static decltype(auto) Call(Args&&... args) {
-        constexpr auto F = std::get<I>(funcs);
-        if constexpr (F == nullptr) {
+    template <class T>
+    inline void DoNotOptimize(T& value) {
+        un_optimizer ^= reinterpret_cast<uint64_t>(&value);
+    }
+
+    void Noop() {
+        int noop = 0;
+        DoNotOptimize(noop);
+    }
+
+    template<typename T>
+    concept Printable = requires(T t) {
+        { t.Print() } -> std::same_as<void>;
+    };
+
+    template <typename T>
+    struct function_traits;
+
+    template <typename Result, typename... Args>
+    struct function_traits<Result(*)(Args...)> {
+        using result_type = Result;
+        using args_tuple = std::tuple<Args...>;
+        static constexpr std::size_t arity = sizeof...(Args);
+    };
+    template <typename Func>
+    using first_arg_t = std::tuple_element_t<0, typename function_traits<std::decay_t<Func>>::args_tuple>;
+
+
+    template<uint16_t Count>
+    struct BenchmarkResults {
+        std::array<std::chrono::nanoseconds, Count> total;
+        std::array<std::chrono::nanoseconds, Count> max;
+        std::array<std::chrono::nanoseconds, Count> min;
+
+        const uint64_t iteration_count;
+
+        BenchmarkResults(uint64_t iteration_count) : iteration_count(iteration_count) {
+            total.fill(std::chrono::nanoseconds(0));
+            max.fill(std::chrono::nanoseconds(0));
+            min.fill(std::chrono::nanoseconds::max());
+        }
+    };
+
+    template<typename Result, auto... Funcs>
+    struct FunctionAbstraction {
+        static constexpr auto funcs = std::tuple{ Funcs... };
+
+        template <std::size_t I, typename... Args>
+        static decltype(auto) Call(Args&&... args) {
+            constexpr auto F = std::get<I>(funcs);
+            if constexpr (F == nullptr) {
                 return;
-        } 
-        else {
-            using FuncResult = std::invoke_result_t<decltype(F), Args...>;
-            if constexpr (std::is_same_v<FuncResult, Result> || std::is_convertible_v<FuncResult, Result>) {
-                if constexpr (std::is_void_v<Result>) {
-                    std::get<I>(funcs)(std::forward<Args>(args)...);
-                    return;
-                } else {
-                    return std::get<I>(funcs)(std::forward<Args>(args)...);
+            }
+            else {
+                using FuncResult = std::invoke_result_t<decltype(F), Args...>;
+                if constexpr (std::is_same_v<FuncResult, Result> || std::is_convertible_v<FuncResult, Result>) {
+                    if constexpr (std::is_void_v<Result>) {
+                        std::get<I>(funcs)(std::forward<Args>(args)...);
+                        return;
+                    }
+                    else {
+                        return std::get<I>(funcs)(std::forward<Args>(args)...);
+                    }
+                }
+                else {
+                    using FirstParamType = first_arg_t<decltype(F)>;
+                    static_assert(std::is_lvalue_reference_v<FirstParamType>, "if the first param isn't the out param, the function needs special handling");
+                    using RetType = std::remove_reference_t<FirstParamType>;
+                    static_assert(std::is_same_v<RetType, Result> || std::is_convertible_v<RetType, Result>, "First param should match the return type");
+                    RetType ret{};
+                    std::get<I>(funcs)(ret, std::forward<Args>(args)...);
+                    return ret;
                 }
             }
-            else{
-                using FirstParamType = first_arg_t<decltype(F)>;
-                static_assert(std::is_lvalue_reference_v<FirstParamType>, "if the first param isn't the out param, the function needs special handling");
-                using RetType = std::remove_reference_t<FirstParamType>;
-                static_assert(std::is_same_v<RetType, Result> || std::is_convertible_v<RetType, Result>, "First param should match the return type");
-                RetType ret{};
-                std::get<I>(funcs)(ret, std::forward<Args>(args)...);
-                return ret;
+        }
+
+        template<std::size_t... Is, typename... Args>
+        static std::array<std::optional<Result>, sizeof...(Funcs)>
+            Call_All_Impl(std::index_sequence<Is...>, Args&&... args) {
+            return { Call<Is>(std::forward<Args>(args)...)... };
+        }
+
+        template<typename... Args>
+        static std::array<std::optional<Result>, sizeof...(Funcs)> Call_All(Args&&... args) {
+            return Call_All_Impl(std::make_index_sequence<sizeof...(Funcs)>{}, std::forward<Args>(args)...);
+        }
+        template<typename... Args>
+        static void Call_All_Print(Args&&... args) {
+            static_assert(Printable<Result>, "attempting to print with a non-printable type");
+            auto results = Call_All_Impl(std::make_index_sequence<sizeof...(Funcs)>{}, std::forward<Args>(args)...);
+            for (auto& ret : results) {
+                if (ret.has_value()) {
+                    ret->Print();
+                }
             }
         }
-    }
-
-    template<std::size_t... Is, typename... Args>
-    static std::array<std::optional<Result>, sizeof...(Funcs)>
-    Call_All_Impl(std::index_sequence<Is...>, Args&&... args) {
-        return { Call<Is>(std::forward<Args>(args)...)... }; 
-    }
-
-    template<typename... Args>
-    static std::array<std::optional<Result>, sizeof...(Funcs)> Call_All(Args&&... args) {
-        return Call_All_Impl(std::make_index_sequence<sizeof...(Funcs)>{}, std::forward<Args>(args)...);
-    }
-    template<typename... Args>
-    static void Call_All_Print(Args&&... args) {
-        static_assert(Printable<Result>, "attempting to print with a non-printable type");
-        auto results = Call_All_Impl(std::make_index_sequence<sizeof...(Funcs)>{}, std::forward<Args>(args)...);
-        for(auto& ret : results){
-            if(ret.has_value()){
-                ret->Print();
+        template<typename PrintFunc, typename... Args>
+        static void Call_All_PrintFunc(PrintFunc&& printFunc, Args&&... args) {
+            auto results = Call_All_Impl(std::make_index_sequence<sizeof...(Funcs)>{}, std::forward<Args>(args)...);
+            for (auto& ret : results) {
+                if (ret.has_value()) {
+                    printFunc(*ret);
+                }
             }
         }
-    }
-    template<typename PrintFunc, typename... Args>
-    static void Call_All_PrintFunc(PrintFunc&& printFunc, Args&&... args) {
-        auto results = Call_All_Impl(std::make_index_sequence<sizeof...(Funcs)>{}, std::forward<Args>(args)...);
-        for(auto& ret : results){
-            if(ret.has_value()){
-                printFunc(*ret);
+
+        template<std::size_t I, typename... Args>
+        static void BenchmarkOne_Time(BenchmarkResults<sizeof...(Funcs)>& results, Args&&... args) {
+            const auto start = std::chrono::high_resolution_clock::now();
+            auto temp = Call<I>(std::forward<Args>(args)...);
+            const auto end = std::chrono::high_resolution_clock::now();
+            DoNotOptimize(temp);
+
+            const auto duration = end - start;
+            results.total[I] += duration;
+            MAX_EQUALS(results.max[I], duration);
+			MIN_EQUALS(results.min[I], duration);
+        }
+
+        template<std::size_t... Is, typename... Args>
+        static void Benchmark_OneEach_Helper_Time(BenchmarkResults<sizeof...(Funcs)>& results, std::index_sequence<Is...>, Args&&... args) {
+            (BenchmarkOne_Time<Is>(results, std::forward<Args>(args)...), ...);
+        }
+
+        template<typename... Args>
+        static BenchmarkResults<sizeof...(Funcs)> Benchmark_OneEach_Time(uint64_t iterationCount, Args&&... args) {
+            BenchmarkResults<sizeof...(Funcs)> results(iterationCount);
+
+            for (uint64_t i = 0; i < iterationCount; ++i) {
+                Benchmark_OneEach_Helper_Time(results, std::make_index_sequence<sizeof...(Funcs)>{}, std::forward<Args>(args)...);
             }
-        }
-    }
 
-    template<std::size_t I, typename... Args>
-    static void BenchmarkOne_Time(BenchmarkResults<sizeof...(Funcs)>& results, Args&&... args) {
-        auto start = std::chrono::high_resolution_clock::now();
-        Call<I>(std::forward<Args>(args)...);
-        auto end = std::chrono::high_resolution_clock::now();
-
-        auto duration = end - start;
-        results.total[I] += duration;
-        results.max[I] = std::max(results.max[I], duration);
-        results.min[I] = std::min(results.min[I], duration);
-    }
-
-    template<std::size_t... Is, typename... Args>
-    static void BenchmarkAll_Time(BenchmarkResults<sizeof...(Funcs)>& results, std::index_sequence<Is...>, Args&&... args) {
-        (BenchmarkOne_Time<Is>(results, std::forward<Args>(args)...), ... );
-    }
-
-    template<typename... Args>
-    static BenchmarkResults<sizeof...(Funcs)> Benchmark_OneEach_Time(uint64_t iterationCount, Args&&... args) {
-        BenchmarkResults<sizeof...(Funcs)> results(iterationCount);
-
-        for (uint64_t i = 0; i < iterationCount; ++i) {
-            BenchmarkAll_Time(results, std::make_index_sequence<sizeof...(Funcs)>{}, std::forward<Args>(args)...);
+            return results;
         }
 
-        return results;
-    }
-};  
+        template<std::size_t I, typename... Args>
+        static void Benchmark_Batch_Time(const uint64_t batchSize, BenchmarkResults<sizeof...(Funcs)>& results, Args&&... args) {
+            const auto start = std::chrono::high_resolution_clock::now();
+
+            for (uint64_t i = 0; i < batchSize; ++i) {
+                if constexpr (std::is_same_v<Result, void>) {
+                    if constexpr (std::get<I>(funcs) != &Noop) {
+                        //compiler warning
+#ifdef _MSC_VER
+                        __pragma(message(__FILE__ "(" TOSTRING(__LINE__)"): warning C4999: Benchmarking a void function (not Noop). Context:" __FUNCSIG__))
+#endif
+                    }
+                    Call<I>(std::forward<Args>(args)...);
+                }
+                else {
+                    auto temp = Call<I>(std::forward<Args>(args)...);
+                    DoNotOptimize(temp);
+                }
+            }
+            const auto end = std::chrono::high_resolution_clock::now();
+
+            const auto duration = std::chrono::duration_cast<std::chrono::nanoseconds>((end - start) / batchSize);
+            results.total[I] += duration;
+            MAX_EQUALS(results.max[I], duration);
+            MIN_EQUALS(results.min[I], duration);
+        }
+        template<std::size_t... Is, typename... Args>
+        static void Benchmark_Batch_Helper_Time(const uint64_t batchSize, BenchmarkResults<sizeof...(Funcs)>& results, std::index_sequence<Is...>, Args&&... args) {
+            (Benchmark_Batch_Time<Is>(batchSize, results, std::forward<Args>(args)...), ...);
+        }
+
+        template<typename... Args>
+        static BenchmarkResults<sizeof...(Funcs)> Benchmark_Batch_Time(uint64_t batchCount, uint64_t batchSize, Args&&... args) {
+            BenchmarkResults<sizeof...(Funcs)> results(batchCount);
+
+            for (uint64_t i = 0; i < batchCount; ++i) {
+                Benchmark_Batch_Helper_Time(batchSize, results, std::make_index_sequence<sizeof...(Funcs)>{}, std::forward<Args>(args)...);
+            }
+
+            return results;
+        }
+    };
+
+
+	using NoopTest = FunctionAbstraction<void, Noop>;
+}
--- src/main.cpp
+++ src/main.cpp
@@ -14,6 +14,7 @@ template <typename T>
 concept Addable = requires(T a, T b) {
     { a + b } -> std::same_as<T>; // require that result is same type T
 };
+
 template<Addable T>
 T AddT(T a, T b) {
     return a + b;
@@ -24,16 +25,20 @@ using VecAbstract3 = TypeAbstraction<lab::Vector<float, 3>, D3DXVECTOR3>;
 using VecAbstract4 = TypeAbstraction<lab::Vector<float, 4>, D3DXVECTOR4>;
 using QuatAbstraction = TypeAbstraction<lab::Quaternion<float>, D3DXQUATERNION>;
 
-using Vec2AddAbst = FunctionAbstraction<VecAbstract2, AddT<lab::Vector<float, 2>>, AddT<D3DXVECTOR2>>;
-using Vec3AddAbst = FunctionAbstraction<VecAbstract3, AddT<lab::Vector<float, 3>>, AddT<D3DXVECTOR3>>;
-using Vec4AddAbst = FunctionAbstraction<VecAbstract4, AddT<lab::Vector<float, 4>>, AddT<D3DXVECTOR4>>;
+using Vec2AddAbst = BATS::FunctionAbstraction<VecAbstract2, AddT<lab::Vector<float, 2>>, AddT<D3DXVECTOR2>>;
+using Vec3AddAbst = BATS::FunctionAbstraction<VecAbstract3, AddT<lab::Vector<float, 3>>, AddT<D3DXVECTOR3>>;
+using Vec4AddAbst = BATS::FunctionAbstraction<VecAbstract4, AddT<lab::Vector<float, 4>>, AddT<D3DXVECTOR4>>;
 
+
+void TestFunc() {
+    int testing = 0;
+}
 
 int main(){
     VecAbstract2 vector2A(lab::Vector<float, 2>{4.f, 1.f});
     VecAbstract2 vector2B(lab::Vector<float, 2>{3.f, 3.f});
 
-
+    
     {
         VecAbstract2 ret = Vec2AddAbst::Call<0>(vector2A, vector2B);
         Print(ret);
@@ -58,11 +63,31 @@ int main(){
     QuatAbstraction quat{lab::Quaternion<float>(0.f, 1.f, 2.f, 3.f)};
     {
         auto results = Vec4AddAbst::Benchmark_OneEach_Time(1000, vec4A, vec4B);
+		printf("\none each results:\n");
         for (auto& ret : results.total) {
 			printf("Total: %lld ns\n", ret.count());
             printf("Avg: %lld ns\n", ret.count() / results.iteration_count);
         }
+    }
+    {
+        auto results = Vec4AddAbst::Benchmark_Batch_Time(100, 10000, vec4A, vec4B);
+		printf("\nbatch results:\n");
+        for (auto& ret : results.total) {
+            printf("Total: %lld ns\n", ret.count());
+            printf("Avg: %lld ns\n", ret.count() / results.iteration_count);
+        }
+    }
+    {
+        auto results = BATS::NoopTest::Benchmark_Batch_Time(100, 1000);
+        printf("\nnoop iters results:\n");
+        for (auto& ret : results.total) {
+            printf("Total: %lld ns\n", ret.count());
+            printf("Avg: %lld ns\n", ret.count() / results.iteration_count);
+        }
+    }
 
+    {
+        auto results = BATS::FunctionAbstraction<void, TestFunc>::Benchmark_Batch_Time(10, 10);
     }
     return EXIT_SUCCESS;
 }
